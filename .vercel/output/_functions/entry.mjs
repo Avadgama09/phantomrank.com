d52f367e419e945caba440eadb409d9963ce0564
Avadgama09 <vadgamaashish09@gmail.com>
2026-06-23 16:13:02 +0530

    chore: add Trustpilot domain verification meta tag to homepage

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DgzRVpuk.mjs';
-import { manifest } from './manifest_BWalbWOZ.mjs';
+import { manifest } from './manifest_B8PVCjAD.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -71,7 +71,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "2a667a0b-4af2-4a1a-bc50-3a77d0757d69",
+    "middlewareSecret": "31cfbad9-b705-4f88-b557-575a3dda2d2f",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
